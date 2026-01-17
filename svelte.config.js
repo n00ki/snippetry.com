ffcb630054998316e9d8c72d77b7155fedbe5669
Noam Shemesh <noams@hey.com>
2026-01-17 20:11:18 +0200

    🩹 bump node version to 24.x

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: 'nodejs24.x'
+    }),
 
     alias: {
       $components: 'src/lib/components',
